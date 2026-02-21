388034810da5379e86e3064b70bd6f28c78c99a9
wiejskachatkapl-stack <wiejskachatka.pl@gmail.com>
2026-02-21 13:00:58 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
-const CACHE_NAME = 'typer-pwa-cache-v1005';
+const CACHE_NAME = 'typer-pwa-cache-v1006';
 const ASSETS = [
   "./",
   "./index.html",
-  "./app.js?v=1005",
+  "./app.js?v=1006",
   "./manifest.json",
   "./img_menu.png",
   "./img_menu_pc.png",
