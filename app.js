f6b987d144103c8871b886e444b798625f8d1368
wiejskachatkapl-stack <wiejskachatka.pl@gmail.com>
2026-02-23 20:30:02 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const BUILD = 3021;
+const BUILD = 3022;
 
 const BG_HOME = "img_menu_pc.png";
 const BG_ROOM = "img_tlo.png";
--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@ const CACHE_NAME = "typer-pwa-cache-v2000";
 const ASSETS = [
   "./",
   "./index.html",
-  "./app.js?v=3021",
+  "./app.js?v=3022",
   "./manifest.json",
   "./ui/flags/pl.png",
   "./ui/flags/gb.png",
