a815778caf9b2a0ad3db18905fdc93d92720e150
wiejskachatkapl-stack <wiejskachatka.pl@gmail.com>
2026-07-20 15:47:45 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // BUILD number shown under the logo (cache-bust + version label)
-const BUILD = 3019;
+const BUILD = 3020;
 const SEASON_ROUNDS = 20;
 const KEY_SEEN_EVENT_PREFIX = "typer_seen_event_v1";
 
