9b387471717692fdfdf2bfab15d8687b7febd608
wiejskachatkapl-stack <wiejskachatka.pl@gmail.com>
2026-07-27 13:52:33 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,16 +1,16 @@
-// Typer PWA Service Worker (BUILD 3074)
-const CACHE_NAME = 'typer-cache-3074';
+// Typer PWA Service Worker (BUILD 3075)
+const CACHE_NAME = 'typer-cache-3075';
 
 // Core assets to pre-cache. leagues.json is intentionally NOT pre-cached,
 // because it should update immediately after edits on GitHub.
 const CORE = [
   './',
   './index.html',
-  './app.js?v=3074',
-  './manifest.json?v=3074',
-  './apple-touch-icon.png?v=3074',
-  './favicon-32x32.png?v=3074',
-  './favicon-16x16.png?v=3074',
+  './app.js?v=3075',
+  './manifest.json?v=3075',
+  './apple-touch-icon.png?v=3075',
+  './favicon-32x32.png?v=3075',
+  './favicon-16x16.png?v=3075',
   './ui/loader_ball.webp',
   './icons/icon-192x192.png',
   './icons/icon-512x512.png'
