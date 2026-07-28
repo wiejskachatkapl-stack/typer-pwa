f3fba5f07788b1ff8393ea3a43f81996cf73a096
wiejskachatkapl-stack <wiejskachatka.pl@gmail.com>
2026-07-28 15:28:16 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// Typer PWA Service Worker (BUILD 3102)
-const BUILD = '3102';
+// Typer PWA Service Worker (BUILD 3103)
+const BUILD = '3103';
 const CACHE_PREFIX = 'typer-cache-';
 const CACHE_NAME = `${CACHE_PREFIX}${BUILD}`;
 const VERSIONED_INDEX = `./index.html?v=${BUILD}`;
